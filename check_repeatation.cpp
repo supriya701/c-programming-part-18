11506d6c91f62c13a97a79e1323a81d6c6be7e22
supriya701 <61152281+supriya701@users.noreply.github.com>
2020-07-05 20:47:46 +0530

    Add files via upload

---
+++ check_repeatation.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int seen[10]={0};
+	int n;
+	printf("enter a no");
+	scanf("%d",&n);
+	
+	int rem;
+	while(n>0)
+	{
+		rem=n%10;
+		if(seen[rem]==1)
+		break;
+		seen[rem]=1;
+		n=n/10;
+		
+	}
+	if(n>0)
+	printf("yes");
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
